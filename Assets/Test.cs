776e554a16df3f90fc589e80954bd2a66cfe3d35
WuHuan <kof1234@sina.com>
2016-09-22 11:00:06 +0800

    测试资源

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,6 +5,7 @@ public class Test : MonoBehaviour
     public GameObject person;
     public Sprite person2;
     public AudioClip person3;
+    public AnimationClip person4;
 
     // Use this for initialization
     void Start () {
